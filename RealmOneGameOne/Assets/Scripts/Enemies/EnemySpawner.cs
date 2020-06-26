51bb23b5a012bf5355b8a7faa4e678af202daa6d
Brent <blebleu4@gmail.com>
2020-06-26 10:37:15 -0500

    Added 3 enemies with animations and Env Art Assets

--- RealmOneGameOne/Assets/Scripts/Enemies/EnemySpawner.cs
+++ RealmOneGameOne/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -6,8 +6,8 @@ public class EnemySpawner : MonoBehaviour
 {
 
     // REMOVE THIS LATER, THIS IS JUST TO TEST ENEMY MOVEMENT
-    public GameObject enemyBasic;
-
+    public GameObject[] enemyList;
+    int enemyToSpawn = 0;
     // public GameObject enemy(other types);
 
     // Start is called before the first frame update
@@ -22,7 +22,9 @@ public class EnemySpawner : MonoBehaviour
         // THIS IS JUST A TEST OF SUMMONING ENEMIES AND GETTING THEM TO RECOGIZE THE MAP LOGIC
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject enemyInstance = Instantiate(enemyBasic,TileMapLogic.getTileBehaviorByGridPosition(1,1).transform.position,Quaternion.identity);
+            enemyToSpawn = Random.Range(0, enemyList.Length);
+
+            GameObject enemyInstance = Instantiate(enemyList[enemyToSpawn],TileMapLogic.getTileBehaviorByGridPosition(1,1).transform.position,Quaternion.identity);
             GameController.enemies.Add(enemyInstance);
         }
     }
